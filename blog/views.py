84e9f1e6207a1968c04710cf1fd1532b5bd6a22c
huanfuan <huanfuan@163.com>
2019-08-15 16:48:05 +0800

    7.2.1 完善模版信息

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,62 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+
+# def post_list(request,category_id=None,tag_id=None):
+#     content='post_list category_id={category_id}, tag_id={tag_id}'.format(
+#         category_id=category_id,
+#         tag_id=tag_id,
+#     )
+#
+#     return HttpResponse(content)
+from blog.models import Tag, Post, Category
+
+
+def post_list(request, category_id=None, tag_id=None):
+    tag = None
+    categoty = None
+
+
+    if tag_id:
+        try:
+            tag=Tag.objects.get(id=tag_id)
+        except Tag.DoesNotExist:
+            post_list=[]
+        else:
+            post_list=tag.post_set.filter(status=Post.STATUS_NORMAL)
+
+    else:
+        post_list=Post.objects.filter(status=Post.STATUS_NORMAL)
+
+        if category_id:
+            try:
+                categoty=Category.objects.get(id=category_id)
+            except Category.DoesNotExist:
+                categoty=None
+
+        else:
+            post_list=post_list.filter(category_id=category_id)
+
+    content = {
+        'category': categoty,
+        'tag': tag,
+        'post_list': post_list,
+    }
+
+    return render(request, 'blog/list.html',context=content)
+
+
+
+
+
+def post_detail(request, post_id=None):
+
+    try:
+        post = Post.objects.get(id=post_id)
+
+    except Post.DoesNotExist:
+        post=None
+
+    return render(request,'blog/detail.html', context={'post':'post'})
--- typeidea/urls.py
+++ typeidea/urls.py
@@ -13,9 +13,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django import views
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path
 
+from blog.views import post_list, post_detail
+from config.views import links
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^$', post_list, name="index"),
+    # url(r'^$', post_list),
+    url(r'^category/(?P<category_id>\d+)/', post_list),
+    url(r'^tag/(?P<tag_id>\d+)/$', post_list),
+    url(r'^post/(?P<post_id>\d+).html$', post_detail),
+    url(r'^links/$', links),
 ]
